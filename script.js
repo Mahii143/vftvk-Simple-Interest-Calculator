ec0cc46afae43279bc3a8542352f7fece860b2ee
Mahii143 <2012091@nec.edu.in>
2021-04-20 11:46:01 +0530

    Half improvement

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+var slider = document.getElementById("range");
+var output = document.getElementById("")
+
 function compute()
 {
     p = document.getElementById("principal").value;
     
 }
+function range()
         
