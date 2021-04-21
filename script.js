2b709d5517ef9d6463d30b655d143338c744b270
Mahii143 <2012091@nec.edu.in>
2021-04-21 14:09:34 +0530

    Fixed Simple Interest decimal value

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
+var slider1 = document.getElementById("rate");
+var output =document.getElementById("values");
+
+output.innerHTML = slider1.value;
+slider1.oninput = function(){
+	output.innerHTML = this.value+" %";
+}
+
 function compute(){	
     var p = document.getElementById("principal").value;
     var r = document.getElementById("rate").value;
 	var n = document.getElementById("years").value;
-	var si = parseInt((p*n*r)/100);
-	var a= parseInt(n,16);
+	var si = p * n * r / 100;
+	var year = new Date().getFullYear()+parseInt(n);;
 	document.querySelector('#num2 mark').textContent = p+",";
 	document.getElementById('num02').innerHTML = "If you deposit ";
 	document.querySelector('#num1 mark').textContent = r+".";
 	document.getElementById ('num01').innerHTML ="at an interest rate of ";
 	document.querySelector('#num mark').textContent = si+",";
 	document.getElementById ('num00').innerHTML = "You will recieve an amount of ";
-	document.querySelector('#num3 mark').textContent = (a*1+2021)+".";
+	document.querySelector('#num3 mark').textContent = year+".";
 	document.getElementById ('num03').innerHTML = "in the year ";
 	return true;
 }
+
 function checkdata()
 {
 	var p = document.getElementById("principal");
-    if (p.value == ""){
-    alert("Please enter the principal amount!");
-    p.focus();
-    return false;}
-    if (p.value <= 0){
-    alert("Please enter a positive amount!");
+    if (p.value <= 0 || p.value == ""){
+    alert("Please enter a positive number!");
     p.focus();
     return false;	
     }
     compute();
     return true;
 }
+
